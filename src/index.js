27cd931f29335991bdcee2b15b84791a1efd36d0
Artemi1Braiko <enti.the.fox@gmail.com>
2026-05-11 00:55:27 +0200

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const reversed = parseInt(Math.abs(n).toString().split('').reverse().join(''));
+  return n < 0 ? -reversed : reversed;
 };
