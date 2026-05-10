838757603e859a3e03445b55c58c6515c5bb0c04
Artemi1Braiko <enti.the.fox@gmail.com>
2026-05-11 00:59:51 +0200

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function reverse(n) {
   const reversed = parseInt(Math.abs(n).toString().split('').reverse().join(''), 10);
-  return n < 0 ? -reversed : reversed;
+  return reversed;
 };
